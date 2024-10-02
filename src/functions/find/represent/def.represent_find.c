1b9ab3c9959ba716548fd8c34b4594d5cda31758
SamuelHenriqueDeMoraisVitrio <samuelhenriquemoraisvitrio@gmail.com>
2024-10-02 08:35:48 -0300

    troca de \n para ''

--- src/functions/find/represent/def.represent_find.c
+++ src/functions/find/represent/def.represent_find.c
@@ -26,10 +26,13 @@ void private_represent_string_array_filtered(CTextStack *self){
     exit(EXIT_FAILURE);
   }
 
+
   printf("\n\tpath: %s\n\n" BLUE, self->rendered_text);
 
   for(int i = 0; i < search.size; i++){
-    printf("context:\t... \"%s\" ...\n", search.searchs[i].context);
+    CTextStack *text_no_formated = ctext.stack.newStack_string(search.searchs[i].context);
+    CTextStack *context_replaced = ctext.stack.replace(text_no_formated, "\n", "");
+    printf("context:\t... \"%s\" ...\n", context_replaced->rendered_text);
     printf("line:\t%ld\n", search.searchs[i].line);
     printf("position:\t%ld\n\n", search.searchs[i].position);
   }
